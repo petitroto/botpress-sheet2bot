c4a8b51e2f5a566275de7df2c4da7b12977d8654
NAKAMURA Masayuki <nakamura@nextis.jp>
2021-05-04 17:07:11 +0900

    chore

--- src/views/lite/index.jsx
+++ src/views/lite/index.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export class AppView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {botId: '', allowOverwrite: false};
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      botId: '',
+      allowOverwrite: false
+    };
     this.fileInput = React.createRef();
     this.axiosConfig = {
       baseURL: '/api/v1/bots/___/mod/sheet2bot',
